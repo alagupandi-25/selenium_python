9ca27ea99ba5209253c087cb4d1bdce3e042a2b6
Alagupandi-25 <ks6925477@gmail.com>
2023-08-25 12:54:41 +0530

    2-commit

--- Selenium_amazon.py
+++ Selenium_amazon.py
@@ -1,41 +1,66 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from time import sleep
-from pprint import pprint
 
-try:
-    
-    title = "Selenium Amazon Search Result"
 
-    print(f"--------------------------------------{title}--------------------------------------")
+def Web_scraping():
+    try:
+        driver = webdriver.Firefox()
+        print("webdriver is open")
 
-    driver = webdriver.Firefox()
-    print("webdriver is open")
+        with open("Search_result.txt", "w", encoding="utf-8") as file:
+            print("Started to the file")
 
-    driver.get("https://www.amazon.in/s?k=gaming+monitor&crid=3MHDYZB2OO82H&sprefix=gaming+monitor%2Caps%2C298&ref=nb_sb_noss_1")
+            file.write("""--------------------------------------Selenium Amazon Search Result--------------------------------------""")
 
-    content = driver.find_elements(By.CSS_SELECTOR,"div.sg-col-20-of-24")
+            driver.get("""https://www.amazon.in/s?k=gaming+monitor&crid=3MHDYZB2OO82H&sprefix=gaming+monitor%2Caps
+            %2C298 &ref=nb_sb_noss_1""")
+            content = driver.find_elements(By.CSS_SELECTOR, "div.sg-col-20-of-24")
 
-    print("\n")
-
-    for ele in content:
-        try:
-
-            text_element = ele.find_element(By.XPATH,".//span[@class='a-size-medium a-color-base a-text-normal']")
-            print("Product Name : ",text_element.text)
+            file.write("\n\n")
             
-            text_element = ele.find_element(By.XPATH,".//span[@class='a-price-whole']")
-            print("Product Price: ",text_element.text)
+            for ele in content:
+                try:
 
-            print("\n")
+                    text_element = ele.find_element(By.XPATH, ".//span[@class='a-size-medium a-color-base a-text-normal']")
+                    file.write("Product Name : "+ text_element.text+"\n")
 
-        except:
-            continue  
+                    text_element = ele.find_element(By.XPATH, ".//span[@class='a-price-whole']")
+                    file.write("Product Price: "+ text_element.text+"\n")
 
-    sleep(4)
+                    file.write("\n")
 
-finally:
-    driver.quit() 
-    print("webdriver is close")
+                except:
+                    continue
+
+            print("Completed the writing file.")
+            print("File located at ", os.path.realpath(file.name))
+
+    except:
+        print("Error Occurred.....")
+
+    finally:
+        driver.quit()
+        print("webdriver is close")
 
 
+if __name__ == "__main__":
+
+    try:
+        while True:
+            title = "Selenium Amazon Search Result"
+            print(f"--------------------------------------{title}--------------------------------------")
+
+            var = input("To run or stop program : ").lower()
+
+            match var:
+                case "run":
+                    Web_scraping()
+                case "stop":
+                    raise KeyboardInterrupt
+                case _:
+                    print("Invalid input.The available input Run or Stop.")
+                    continue
+
+    except KeyboardInterrupt:
+        print(" End of Program.")
